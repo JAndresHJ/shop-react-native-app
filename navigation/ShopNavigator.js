ef3494d278b3f9a91c55e2ebdbff67d1e51572d7
Jorge Andrés Hernández Jara <39096718+JAndresHJ@users.noreply.github.com>
2020-06-26 14:05:01 -0500

    Implements cart screen

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -3,17 +3,19 @@ import { createAppContainer } from 'react-navigation';
 import { Platform } from 'react-native';
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
+import CartScreen from '../screens/shop/CartScreen';
 import Colors from '../constants/Colors';
 
 const ProductsNavigator = createStackNavigator(
   {
     ProductsOverview: ProductsOverviewScreen,
-    ProductDetail: ProductDetailScreen
+    ProductDetail: ProductDetailScreen,
+    Cart: CartScreen
   },
   {
     defaultNavigationOptions: {
       headerStyle: {
-        backgroundColor: Platform.OS ==='android' ? Colors.primary : ''
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
       },
       headerTitleStyle: {
         fontFamily: 'open-sans-bold'
@@ -21,8 +23,8 @@ const ProductsNavigator = createStackNavigator(
       headerBackTitleStyle: {
         fontFamily: 'open-sans'
       },
-      headerTintColor: Platform.OS ==='android' ? 'white' : Colors.primary,
-    },
+      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+    }
   }
 );
 
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { FlatList, Text } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import ProductItem from '../../components/shop/ProductItem';
-import * as cartActions from '../../store/actions/cart'
+import * as cartActions from '../../store/actions/cart';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import HeaderButton from '../../components/UI/HeaderButton';
 
 const ProductsOverviewScreen = (props) => {
   const products = useSelector((state) => state.products.availableProducts);
@@ -20,11 +22,11 @@ const ProductsOverviewScreen = (props) => {
           onViewDetail={() =>
             props.navigation.navigate('ProductDetail', {
               productId: itemData.item.id,
-              productTitle: itemData.item.title
+              productTitle: itemData.item.title,
             })
           }
           onAddToCart={() => {
-            dispatch(cartActions.addToCart(itemData.item))
+            dispatch(cartActions.addToCart(itemData.item));
           }}
         />
       )}
@@ -32,8 +34,21 @@ const ProductsOverviewScreen = (props) => {
   );
 };
 
-ProductsOverviewScreen.navigationOptions = {
-  headerTitle: 'All Products',
+ProductsOverviewScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: 'All Products',
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Cart"
+          iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+          onPress={() => {
+            navData.navigation.navigate('Cart');
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
 };
 
 export default ProductsOverviewScreen;
