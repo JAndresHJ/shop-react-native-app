f4fbdc2cddac781fae5a78ac044b3fb665130e6b
Jorge Andrés Hernández Jara <39096718+JAndresHJ@users.noreply.github.com>
2020-07-13 08:21:56 -0500

    FIX: keyboard overlaps input

--- screens/user/EditProductsScreen.js
+++ screens/user/EditProductsScreen.js
@@ -118,7 +118,7 @@ const EditProductScreen = (props) => {
   );
 
   return (
-    <KeyboardAvoidingView style={{flex: 1}} behavior="padding" keyboardVerticalOffset={100}>
+    <KeyboardAvoidingView style={{flex: 1}} behavior="padding" keyboardVerticalOffset={10}>
       <ScrollView>
         <View style={styles.form}>
           <Input
